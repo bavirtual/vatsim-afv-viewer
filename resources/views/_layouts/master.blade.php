e05e2bda1e7230873832bcb1419ff16f0bbc8a44
Matt Bozwood-Davies <matt.bozwooddavies@btinternet.com>
2019-04-29 22:51:14 +0100

    powered by bav

--- resources/views/_layouts/master.blade.php
+++ resources/views/_layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="BAVMS, the core of BAVirtual">
+    <meta name="description" content="AFV Viewer - Powered by BAVirtual">
     <meta name="keywords" content="bavirtual,ba,virtual airline">
     <meta name="author" content="Matt Bozwood-Davies">
     <meta name="csrf-token" content="{{ csrf_token() }}">
