63e8d9526078b3d4213dd0eeae29e9196f2abbfe
Matt Bozwood-Davies <matt.bozwooddavies@btinternet.com>
2019-04-29 22:49:21 +0100

    empty index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,13 +7,7 @@
 
     <div class="row">
         <div class="col-md-12">
-            <section class="card">
-                <div class="card-content">
-                    <div class="card-body" id="atisList" style="height: calc(100vh - 300px);">
 
-                    </div>
-                </div>
-            </section>
         </div>
     </div>
 
