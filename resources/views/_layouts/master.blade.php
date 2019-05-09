75304c8d157f6ae688448250092b67368d8ec26d
Matt Bozwood-Davies <matt.bozwooddavies@btinternet.com>
2019-05-09 20:48:15 +0100

    Change footer

--- resources/views/_layouts/master.blade.php
+++ resources/views/_layouts/master.blade.php
@@ -105,7 +105,7 @@
 
 <footer class="footer fixed-bottom footer-dark navbar-shadow">
     <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2">
-        <span class="float-md-left d-block d-md-inline-block">Copyright &copy; {{ date('Y') }} <a class="text-bold-800 grey darken-2" href="https://bavirtual.co.uk" target="_blank">BAVirtual </a>, All rights reserved. </span>
+        <span class="float-md-left d-block d-md-inline-block">Copyright &copy; {{ date('Y') }} <a class="text-bold-800 grey darken-2" href="https://www.vatsim.net" target="_blank">VATSIM.net</a>. Technology provided by <a class="text-bold-800 grey darken-2" href="https://bavirtual.co.uk" target="_blank">BAVirtual</a>, All rights reserved. </span>
     </p>
 </footer>
 
