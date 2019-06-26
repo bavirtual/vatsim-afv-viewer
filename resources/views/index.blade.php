9d6150f2f0b651aed47a59b322d7a212367b4111
Matt Bozwood-Davies <matt.bozwooddavies@gmail.com>
2019-06-26 18:32:35 +0100

    functionise

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,116 +56,7 @@
         mapMarkers = [];
         onlineTransceivers = [];
         clients = [];
-        $.ajax({
-            type: 'get',
-            url: '{{ route('atis-map-data') }}',
-            success: function(data) {
-                data.forEach(function (client) {
-                    client['transceivers'].forEach(function (transceiver) {
-                        var callsign = client.callsign;
-                        var name = client.member_name;
-                        var frequency = transceiver.frequency;
-                        frequency = (frequency/1000000).toFixed(3);
-                        var lat = transceiver.latDeg;
-                        var lon = transceiver.lonDeg;
-                        var msl = transceiver.altMslM;
-
-                        var content = '<b>' + callsign +'</b><br>';
-                        content += '<b>' + name +'</b><br>';
-                        if(client.type == 'pilot') {
-                            content += client.altitude + 'ft<br>';
-                            content += client.route + '<br>';
-                        }
-                        content += frequency;
-
-                        if(client.type == 'atis') {
-                            var marker = L.marker([lat, lon], {
-                                icon: L.icon({
-                                    iconUrl: '{{ asset_path('img/map/pin.png') }}',
-                                    iconSize: [10, 17],
-                                    iconAnchor: [5, 17],
-                                    popupAnchor: [0, -17]
-                                }),
-                            });
-                        // } else if (callsign.includes('_DEL') || callsign.includes('_GND') || callsign.includes('_TWR') || callsign.includes('_APP') || callsign.includes('_DEP') || callsign.includes('_CTR') || callsign.includes('_FSS')) { // controller
-                        } else if(client.type == 'controller') { // controller
-                            var marker = L.marker([lat, lon], {
-                                icon: L.icon({
-                                    iconUrl: '{{ asset_path('img/map/green-pin.png') }}',
-                                    iconSize: [10, 16],
-                                    iconAnchor: [5, 16],
-                                    popupAnchor: [0, -16]
-                                }),
-                            });
-                        } else { // plane
-                            if(client.heading == 'N/A') {
-                                var marker = L.marker([lat, lon], {
-                                    icon: L.icon({
-                                        iconUrl: '{{ asset_path('img/map/plane.png') }}',
-                                        iconSize: [30, 30],
-                                        iconAnchor: [15, 15],
-                                        popupAnchor: [0, -15]
-                                    }),
-                                });
-                            } else {
-                                var marker = L.marker([lat, lon], {
-                                    icon: L.icon({
-                                        iconUrl: '{{ asset_path('img/map/plane.png') }}',
-                                        iconSize: [30, 30],
-                                        iconAnchor: [15, 15],
-                                        popupAnchor: [0, -15]
-                                    }),
-                                    rotationAngle: client.heading,
-                                    rotationOrigin: 'center'
-                                });
-                            }
-                        }
-
-                        if (callsign.includes('_DEL') || callsign.includes('_GND') || callsign.includes('_TWR') || callsign.includes('_APP') || callsign.includes('_DEP') || callsign.includes('_CTR') || callsign.includes('_FSS')) {
-                            var RadiusMeters = (1.25 * Math.sqrt(msl * 3.28084)) * 1852;
-                            L.circle([lat, lon], {radius: RadiusMeters, fillOpacity: 0, color: '#418041'}).addTo(map);
-                        } else if (!callsign.includes('_')) {
-                            var RadiusMeters = (1.25 * Math.sqrt(msl * 3.28084)) * 1852;
-                            L.circle([lat, lon], {radius: RadiusMeters, fillOpacity: 0, color: '#ce6262'}).addTo(map);
-                        }
-
-                        marker.addTo(map).bindPopup(content);
-                        mapMarkers.push(marker);
-                        onlineTransceivers.push({callsign: callsign, freq: frequency});
-                    });
-                });
-
-                if(mapMarkers.length > 0) {
-                    map.fitBounds(L.featureGroup(mapMarkers).getBounds());
-                    onlineTransceivers.sort(function(a, b) {
-                        return ((a.callsign < b.callsign) ? -1 : ((a.callsign > b.callsign) ? 1 : 0));
-                    });
-
-                    // [{client: '', frequencies:['123.000','122.800']]]
-                    onlineTransceivers.forEach(function(onlineTransceiver) {
-                        var clientIndex = clients.findIndex(x => x.callsign === onlineTransceiver.callsign);
-                        if(clientIndex > -1) {
-                            // add the frequency to array
-                            var client = clients.find(x => x.callsign === onlineTransceiver.callsign);
-                            clients.splice(clientIndex, 1);
-                            client['frequencies'].push(onlineTransceiver.freq);
-                            clients.push(client);
-                        } else {
-                            // create a new element
-                            clients.push({callsign: onlineTransceiver.callsign, frequencies: [onlineTransceiver.freq]});
-                        }
-                    });
-
-                    clients.forEach(function (client) {
-                        $('#atis-list').append('<h5 style="margin-bottom: 0;">' + client.callsign + ' - ' + client['frequencies'].join(',') + '</h5>');
-                    });
-                    $('#online-count').html(clients.length + ' Voice Clients Connected');
-                } else {
-                    $('#atisList').append('<h5 style="margin-bottom: 0;">No Voice Clients</h5>');
-                    $('#online-count').html('0 Voice Clients Connected');
-                }
-            }
-        });
+        reloadMapData();
 
         $('#togglePilotRings').click(function () {
             if(pilotRings == true) {
@@ -188,17 +79,15 @@
         });
 
         setInterval(function() {
+            reloadMapData();
+        }, 15000);
+        
+        
+        function reloadMapData() {
             $.ajax({
                 type: 'get',
                 url: '{{ route('atis-map-data') }}',
-                success: function (data) {
-                    for (var i = 0; i < mapMarkers.length; i++) {
-                        map.removeLayer(mapMarkers[i]);
-                    }
-                    $('path.leaflet-interactive').remove();
-                    clients = [];
-                    onlineTransceivers = [];
-                    $('#atis-list').html('');
+                success: function(data) {
                     data.forEach(function (client) {
                         client['transceivers'].forEach(function (transceiver) {
                             var callsign = client.callsign;
@@ -226,7 +115,7 @@
                                         popupAnchor: [0, -17]
                                     }),
                                 });
-                                // } else if (callsign.includes('_DEL') || callsign.includes('_GND') || callsign.includes('_TWR') || callsign.includes('_APP') || callsign.includes('_DEP') || callsign.includes('_CTR') || callsign.includes('_FSS')) { // controller
+                            // } else if (callsign.includes('_DEL') || callsign.includes('_GND') || callsign.includes('_TWR') || callsign.includes('_APP') || callsign.includes('_DEP') || callsign.includes('_CTR') || callsign.includes('_FSS')) { // controller
                             } else if(client.type == 'controller') { // controller
                                 var marker = L.marker([lat, lon], {
                                     icon: L.icon({
@@ -274,15 +163,8 @@
                         });
                     });
 
-                    if(pilotRings == 0) {
-                        $('path[stroke="#ce6262"]').hide();
-                    }
-
-                    if(atcRings == 0) {
-                        $('path[stroke="#418041"]').hide();
-                    }
-
                     if(mapMarkers.length > 0) {
+                        map.fitBounds(L.featureGroup(mapMarkers).getBounds());
                         onlineTransceivers.sort(function(a, b) {
                             return ((a.callsign < b.callsign) ? -1 : ((a.callsign > b.callsign) ? 1 : 0));
                         });
@@ -307,12 +189,12 @@
                         });
                         $('#online-count').html(clients.length + ' Voice Clients Connected');
                     } else {
-                        $('#atis-list').append('<h5 style="margin-bottom: 0;">No Voice Clients</h5>');
+                        $('#atisList').append('<h5 style="margin-bottom: 0;">No Voice Clients</h5>');
                         $('#online-count').html('0 Voice Clients Connected');
                     }
                 }
             });
-        }, 15000);
+        }
 
     </script>
 
