8ffe04bcc141080979af21c28aeb206d6e8070af
Matt Bozwood-Davies <matt.bozwooddavies@btinternet.com>
2019-10-19 20:20:34 +0100

    break out unicom

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -8,17 +8,80 @@ class DetailController extends Controller
 {
     public function data()
     {
-        $unicom = $this->unicom();
-        $unicomCount = 0;
-        $morethan1 = false;
-        foreach($unicom as $uni) {
-            if($uni > 1) {
-                $morethan1 = true;
+        $afvData = AfvApiController::getClients();
+        $unicomTransceivers = [];
+        $unicomWestNATransceivers = [];
+        $unicomEastNASATransceivers = [];
+        $unicomEUTransceivers = [];
+        $unicomOtherTransceivers = [];
+
+        $unicomTransceiverCount = 0;
+        $unicomWestNATransceiverCount = 0;
+        $unicomEastNASATransceiverCount = 0;
+        $unicomEUTransceiverCount = 0;
+        $unicomOtherTransceiverCount = 0;
+
+        foreach($afvData as $callsign => $afvDatum) {
+            foreach($afvDatum['transceivers'] as $transceiver) {
+                if($transceiver->frequency == 122800000) {
+                    if(isset($unicomTransceivers[$callsign])) {
+                        $unicomTransceivers[$callsign] = $unicomTransceivers[$callsign] + 1;
+                    } else {
+                        $unicomTransceivers[$callsign] = 1;
+                    }
+                }
+                if($transceiver->frequency == 122800001) {
+                    if(isset($unicomWestNATransceivers[$callsign])) {
+                        $unicomWestNATransceivers[$callsign] = $unicomWestNATransceivers[$callsign] + 1;
+                    } else {
+                        $unicomWestNATransceivers[$callsign] = 1;
+                    }
+                }
+                if($transceiver->frequency == 122800002) {
+                    if(isset($unicomEastNASATransceivers[$callsign])) {
+                        $unicomEastNASATransceivers[$callsign] = $unicomEastNASATransceivers[$callsign] + 1;
+                    } else {
+                        $unicomEastNASATransceivers[$callsign] = 1;
+                    }
+                }
+                if($transceiver->frequency == 122800003) {
+                    if(isset($unicomEUTransceivers[$callsign])) {
+                        $unicomEUTransceivers[$callsign] = $unicomEUTransceivers[$callsign] + 1;
+                    } else {
+                        $unicomEUTransceivers[$callsign] = 1;
+                    }
+                }
+                if($transceiver->frequency == 122800004) {
+                    if(isset($unicomOtherTransceivers[$callsign])) {
+                        $unicomOtherTransceivers[$callsign] = $unicomOtherTransceivers[$callsign] + 1;
+                    } else {
+                        $unicomOtherTransceivers[$callsign] = 1;
+                    }
+                }
             }
-            $unicomCount = $unicomCount + $uni;
         }
 
-        return view('details', compact('unicom','unicomCount','morethan1'));
+        foreach($unicomTransceivers as $unicomTransceiver) {
+            $unicomTransceiverCount = $unicomTransceiverCount + $unicomTransceiver;
+        }
+
+        foreach($unicomWestNATransceivers as $westNATransceiver) {
+            $unicomWestNATransceiverCount = $unicomWestNATransceiverCount + $westNATransceiver;
+        }
+
+        foreach($unicomEastNASATransceivers as $eastNASATransceiver) {
+            $unicomEastNASATransceiverCount = $unicomEastNASATransceiverCount + $eastNASATransceiver;
+        }
+
+        foreach($unicomEUTransceivers as $EUTransceiver) {
+            $unicomEUTransceiverCount = $unicomEUTransceiverCount + $EUTransceiver;
+        }
+
+        foreach($unicomOtherTransceivers as $otherTransceiver) {
+            $unicomOtherTransceiverCount = $unicomOtherTransceiverCount + $otherTransceiver;
+        }
+
+        return view('details', compact('unicomWestNATransceivers','unicomEastNASATransceivers', 'unicomEUTransceivers', 'unicomOtherTransceivers', 'unicomWestNATransceiverCount', 'unicomEastNASATransceiverCount', 'unicomEUTransceiverCount', 'unicomOtherTransceiverCount', 'unicomTransceivers', 'unicomTransceiverCount'));
     }
 
     public function unicom()
--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -12,17 +12,47 @@
             <section class="card" style="height: calc(100% - 1.875rem);">
                 <div class="card-content h-100 bg-dark rounded">
                     <div class="card-body h-100 bg-dark rounded">
-                        <h5 style="color: white">Unicom Transceiver Count: {{ $unicomCount }}</h5>
-                        @if($morethan1)
-                            <h5 style="color: white">The following client(s) have more than 1 UNICOM transceiver:</h5>
-                            <ul style="color: white;">
-                                @foreach($unicom as $callsign => $count)
-                                    @if($count > 1)
-                                        <li style="color: white;">{{ $callsign }} - {{ $count }}</li>
-                                    @endif
-                                @endforeach
-                            </ul>
-                        @endif
+                        <h5 style="color: white">TOTAL Unicom Transceiver Count: {{ $unicomTransceiverCount + $unicomWestNATransceiverCount + $unicomEastNASATransceiverCount + $unicomEUTransceiverCount + $unicomOtherTransceiverCount}}</h5>
+                        <hr>
+                        <h5 style="color: white">Regular Unicom Transceiver Count: {{ $unicomTransceiverCount }}</h5>
+                        <h5 style="color: white">The following client(s) have more than 1 Regular UNICOM Transceiver:</h5>
+                        <ul style="color: white;">
+                            @foreach($unicomTransceivers as $callsign => $count)
+                                @if($count > 1) <li style="color: white;">{{ $callsign }} - {{ $count }}</li> @endif
+                            @endforeach
+                        </ul>
+                        <hr>
+                        <h5 style="color: white">Unicom West NA Transceiver Count: {{ $unicomWestNATransceiverCount }}</h5>
+                        <h5 style="color: white">The following client(s) have more than 1 Unicom West NA Transceiver:</h5>
+                        <ul style="color: white;">
+                            @foreach($unicomWestNATransceivers as $callsign => $count)
+                                @if($count > 1) <li style="color: white;">{{ $callsign }} - {{ $count }}</li> @endif
+                            @endforeach
+                        </ul>
+                        <hr>
+                        <h5 style="color: white">Unicom East NA and SA Transceiver Count: {{ $unicomEastNASATransceiverCount }}</h5>
+                        <h5 style="color: white">The following client(s) have more than 1 Unicom East NA and SA Transceiver:</h5>
+                        <ul style="color: white;">
+                            @foreach($unicomEastNASATransceivers as $callsign => $count)
+                                @if($count > 1) <li style="color: white;">{{ $callsign }} - {{ $count }}</li> @endif
+                            @endforeach
+                        </ul>
+                        <hr>
+                        <h5 style="color: white">Unicom EU Transceiver Count: {{ $unicomEUTransceiverCount }}</h5>
+                        <h5 style="color: white">The following client(s) have more than 1 Unicom EU Transceiver:</h5>
+                        <ul style="color: white;">
+                            @foreach($unicomEUTransceivers as $callsign => $count)
+                                @if($count > 1) <li style="color: white;">{{ $callsign }} - {{ $count }}</li> @endif
+                            @endforeach
+                        </ul>
+                        <hr>
+                        <h5 style="color: white">Unicom Other Transceiver Count: {{ $unicomOtherTransceiverCount }}</h5>
+                        <h5 style="color: white">The following client(s) have more than 1 UNICOM Other Transceiver:</h5>
+                        <ul style="color: white;">
+                            @foreach($unicomOtherTransceivers as $callsign => $count)
+                                @if($count > 1) <li style="color: white;">{{ $callsign }} - {{ $count }}</li> @endif
+                            @endforeach
+                        </ul>
                     </div>
                 </div>
             </section>
